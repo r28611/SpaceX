76488a52427540eee7040787e38ad33d05639d0c
Margarita Novokh <r28611@gmail.com>
2022-09-08 16:21:12 +0200

    Initial Commit

---
+++ SpaceXLaunches/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceXLaunches
+//
+//  Created by Margarita Novokhatskaia on 08/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
